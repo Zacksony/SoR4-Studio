edcbbba0986bc2ce95e103cd79a8ddac5e746e16
Zacksony <Zxyth123@163.com>
2023-12-28 18:22:45 +0800

    vis : 微调outlined元素外观

---
+++ Modules/Windows/Tabs/LevelOrderEdit.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SoR4_Studio.Modules.Windows.Tabs
+{
+    /// <summary>
+    /// LevelOrderEdit.xaml 的交互逻辑
+    /// </summary>
+    public partial class LevelOrderEdit : UserControl
+    {
+        public LevelOrderEdit()
+        {
+            InitializeComponent();
+        }
+    }
+}
